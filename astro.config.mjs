b2375ae2e369d1da088b51df31b868225e31eb5f
Daniel Joo <dsj2122@columbia.edu>
2026-02-03 01:13:09 -0500

    fix the css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danielsjoo.github.io',
-  base: import.meta.env.DEV ? '/' : '/footprints_landing/',
+  base: '/footprints_landing/',
   vite: {
     plugins: [tailwindcss()]
   }
