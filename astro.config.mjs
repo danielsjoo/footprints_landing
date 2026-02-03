3490b1d69f427c894bef9d7ff1783cb10ee83ff7
Daniel Joo <dsj2122@columbia.edu>
2026-02-03 00:57:03 -0500

    fixed path bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://danielsjoo.github.io',
+  base: import.meta.env.DEV ? '/' : '/footprints_landing/',
   vite: {
     plugins: [tailwindcss()]
   }
